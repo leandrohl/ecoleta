b3bc5c076e7a58547bbe63f5b04cabe942f04f4a
Leandro Henrique <leandro.henrique@unesp.br>
2020-06-03 14:45:34 -0300

    contrução do html da primeira página

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 
 // JSX: SINTAXE DE XML(html) DENTRO DO JAVASCRIPT
 // COMPONENTE: tudo aquilo que pode replicar na pagina
 // obs: todo componente comeca com letra maiuscula
-import Header from './Header';
+
+import Home from './pages/Home';
 
 function App() {
-  let [counter, setCounter] = useState(0); // [valor do estado, função para atualizar o valor do estado]
-  function handleButtonClick(){
-    setCounter(counter + 1); 
-  }
-
   return (
       <div>
-        <Header title = "Hello World"/> 
-
-        <h1>{counter}</h1>
-        <button type="button" onClick={handleButtonClick}>Aumentar</button>
+        <Home />
       </div>
   );
 }
