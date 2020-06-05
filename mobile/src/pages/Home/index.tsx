21785f6347e2c499e1836fc01ee704e9a1d97240
Leandro Henrique <leandro.henrique@unesp.br>
2020-06-05 19:01:25 -0300

    busca de estado e cidade concluida

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,44 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Feather as Icon} from '@expo/vector-icons'
-import { View, ImageBackground, Image, Text, StyleSheet } from 'react-native';
+import { View, TextInput, ImageBackground, KeyboardAvoidingView, Image, Text, StyleSheet, Platform } from 'react-native';
 import { RectButton} from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native';
 
+
 const Home = () => {
 	const navigation = useNavigation();
+	const [uf, setUf] = useState('');
+	const [city, setCity] = useState('');
+
 
 	function handleNavigateToPoints(){
-		navigation.navigate('Points');
+		navigation.navigate('Points', {
+			uf, 
+			city,
+		})
 	}
 
 	return (
-		<ImageBackground 
-		source={require("../../assets/home-background.png")} 
-		style={styles.container}
-		imageStyle = {{ width: 274, height: 368}}>
-			<View style={styles.main}>
-				<Image source={require('../../assets/logo.png')} />
-				<Text style={styles.title}>
-					Seu markplace de coleta de resíduos
-				</Text>
-				<Text style={styles.description}>
-					Ajudamoss pessoas a encontrarem pontos de coleta de forma eficiente.
-				</Text>
-			</View>
-
-			<View style={styles.footer}>
-				<RectButton style={styles.button} onPress={handleNavigateToPoints}>
-					<View style={styles.buttonIcon}>
-						<Text>
-							<Icon name="arrow-right" color="#FFF" size={24} />
-						</Text>
-					</View>	
-					<Text style={styles.buttonText}>
-						Entrar
+		<KeyboardAvoidingView style={{flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding': undefined}>
+			<ImageBackground 
+			source={require("../../assets/home-background.png")} 
+			style={styles.container}
+			imageStyle = {{ width: 274, height: 368}}>
+				<View style={styles.main}>
+					<Image source={require('../../assets/logo.png')} />
+					<Text style={styles.title}>
+						Seu markplace de coleta de resíduos
 					</Text>
-				</RectButton>
-			</View>
-		</ImageBackground>
+					<Text style={styles.description}>
+						Ajudamoss pessoas a encontrarem pontos de coleta de forma eficiente.
+					</Text>
+				</View>
+
+				<View style={styles.footer}>
+					<TextInput
+					style={styles.input}
+					placeholder="Digite a UF"
+					value = {uf}
+					onChangeText={setUf}
+					autoCapitalize="characters"
+					autoCorrect={false}
+					>
+					</TextInput>
+					<TextInput
+					style={styles.input}
+					placeholder="Digite a cidade"
+					value = {city}
+					onChangeText={setCity}
+					autoCorrect={false}
+					>
+					</TextInput>
+					<RectButton style={styles.button} onPress={handleNavigateToPoints}>
+						<View style={styles.buttonIcon}>
+							<Text>
+								<Icon name="arrow-right" color="#FFF" size={24} />
+							</Text>
+						</View>	
+						<Text style={styles.buttonText}>
+							Entrar
+						</Text>
+					</RectButton>
+				</View>
+			</ImageBackground>
+		</KeyboardAvoidingView>
 	);
 };
 
